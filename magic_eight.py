d553a9ec111660fd6199aea19bd451996f98428d
stemlatina <md3534@nyu.edu>
2019-09-23 12:24:11 -0400

    First commit

---
+++ magic_eight.py
@@ -0,0 +1,18 @@
+#Marilu D. & Alison S.
+#SI 507
+#create a function that asks users “What is your question?” (or something similar) and saves the response
+
+
+class Magic8:
+
+	def __init__(self, question):
+		self.question = question
+		pass
+
+	def ask_question(self, question):
+		question_list = []
+		q = self.question
+		question_list.append(q)
+		pass
+
+  q = Magic8(input("What is your question? :   "))
