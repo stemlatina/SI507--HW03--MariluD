166afd5a37c4718d2c4e3d948e05993208b841ca
stemlatina <md3534@nyu.edu>
2019-09-24 06:39:00 -0400

    code

--- magic_eight.py
+++ magic_eight.py
@@ -1,7 +1,8 @@
-#Marilu D. & Alison S.
-#SI 507
-#create a function that asks users “What is your question?” (or something similar) and saves the response
+# SI 507 Fall 2019
+# Homework 3 - Magic 8-Ball Code & Git Tutorial
+# Worked on by: Marilu D. and Alison S.
 
+import random
 
 class Magic8:
 
@@ -15,4 +16,30 @@ class Magic8:
 		question_list.append(q)
 		pass
 
-  q = Magic8(input("What is your question? :   "))
+	def pick_random_ans(self):
+
+		#List from Wiki: https://en.wikipedia.org/wiki/Magic_8-Ball
+		rand_list = ["It is certain", "It is decidedly so","Without a doubt", "Yes - definitely","You may rely on it", "As I see it, yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you now","Cannot predict now","Concentrate and ask again", "Don't count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"]
+
+		rand_item = random.choice(rand_list)
+		r = str(rand_item)
+
+		return r
+
+	def check_for_question(self, question):
+
+		question = self.question
+		q = question[-1:]
+
+		r = self.pick_random_ans()
+
+		if q == "?":
+			return print("The Magic 8 ball says:  " + r)
+		else:
+			return print("I'm sorry, I can only answer questions.  Please enter a question.")
+
+
+
+q = Magic8(input("What is your question? :   "))
+qq = q.question
+q.check_for_question(qq)
