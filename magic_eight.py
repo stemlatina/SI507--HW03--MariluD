1d4dbb2b45950417730b80bc021d2c216630746d
awsummer <awsummer@umich.edu>
2019-09-23 20:48:59 -0400

    adding revised magic_eight

--- magic_eight.py
+++ magic_eight.py
@@ -1,7 +1,4 @@
-#Marilu D. & Alison S.
-#SI 507
-#create a function that asks users “What is your question?” (or something similar) and saves the response
-
+import random
 
 class Magic8:
 
@@ -15,4 +12,15 @@ class Magic8:
 		question_list.append(q)
 		pass
 
-  q = Magic8(input("What is your question? :   "))
+	def pick_random_ans(self):
+
+		#List from Wiki: https://en.wikipedia.org/wiki/Magic_8-Ball
+		rand_list = ["It is certain", "It is decidedly so","Without a doubt", "Yes - definitely","You may rely on it", "As I see it, yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you now","Cannot predict now","Concentrate and ask again", "Don't count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"]
+
+		rand_item = random.choice(rand_list)
+		r = str(rand_item)
+
+		return r
+
+q = Magic8(input("What is your question? :   "))
+print(q.pick_random_ans())
